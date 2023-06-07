e09f6ebd77510d44b12fbcff7e25e739470eda7b
Alexis <romelitis@hotmail.com>
2023-06-06 21:50:23 -0600

    Everything works fine

--- pokeapp/src/main.tsx
+++ pokeapp/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import Homepage from "./pages/homepage";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Homepage from "./pages/Homepage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- pokeapp/src/pages/Homepage.tsx
+++ pokeapp/src/pages/Homepage.tsx
@@ -1,7 +1,72 @@
+import { useState } from "react";
+
 export default function Homepage() {
+  const [pokemon, setPokemon] = useState([]);
+  const [name, setName] = useState([]);
+  const [status, setStatus] = useState([]);
+
+  const [submitName, setSubmitName] = useState("");
+
+  const handleSubmit = (event: Event) => {
+    event.preventDefault();
+    setSubmitName("");
+    console.log(submitName);
+  };
+  const getPokemon = () => {
+    fetch("https://rickandmortyapi.com/api/character/" + submitName)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setPokemon(json.image);
+        setName(json.name);
+        setStatus(json.status);
+      });
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Rick and Morty Characters</h1>
+      <img
+        src="https://www.pngkit.com/png/full/112-1128909_rick-and-morty-transparent-png-fondos-de-pantalla.png"
+        className="img-fluid"
+        alt="..."
+      ></img>
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3 hello bigtext">
+          <label /* htmlFor="exampleInputEmail1" */ className="form-label">
+            Enter a number
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(event) => {
+              setSubmitName(event.target.value);
+            }}
+          />
+
+          <div id="emailHelp" className="form-text">
+            Pick a number from 1 ~ 826
+          </div>
+          <input
+            value="Search"
+            onClick={() => {
+              getPokemon();
+            }}
+            className="btn btn-primary"
+            type="submit"
+          />
+        </div>
+      </form>
+      <img className="border border-black picture" src={pokemon} alt="" />
+      <h2>
+        <b>Name:</b> {name}
+      </h2>
+      <h2>
+        <b>Status:</b> {status}
+      </h2>
     </>
   );
 }
